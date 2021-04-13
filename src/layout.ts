20281b51c1e40b881816e0bb4ef4223485362bdf
Manassarn Manoonchai <manassarn@gmail.com>
2021-04-13 20:34:31 +0700

    refactor: add type for layout matrix

--- src/layout.ts
+++ src/layout.ts
@@ -2,30 +2,31 @@ interface LayoutOptions {
   name: "pattachote" | "kedmanee";
 }
 
-const LAYOUTS = {
-  kedmanee: [
-    ["ๆ", "ไ", "ำ", "พ", "ะ", "ั", "ี", "ร", "น", "ย", "บ", "ล", "ฃ"],
-    ["ฟ", "ห", "ก", "ด", "เ", "้", "่", "า", "ส", "ว", "ง"],
-    ["ผ", "ป", "แ", "อ", "ิ", "ื", "ท", "ม", "ใ", "ฝ"],
-  ],
+type ILayoutMatrix = {
+  [name in LayoutOptions["name"]]: string[][];
+};
+
+const LAYOUTS: ILayoutMatrix = {
   pattachote: [
     ["็", "ต", "ย", "อ", "ร", "่", "ด", "ม", "ว", "แ", "ใ", "ฌ", "ฃ"],
     ["้", "ท", "ง", "ก", "ั", "ี", "า", "น", "เ", "ไ", "ข"],
     ["บ", "ป", "ล", "ห", "ิ", "ค", "ส", "ะ", "จ", "พ"],
   ],
+  kedmanee: [
+    ["ๆ", "ไ", "ำ", "พ", "ะ", "ั", "ี", "ร", "น", "ย", "บ", "ล", "ฃ"],
+    ["ฟ", "ห", "ก", "ด", "เ", "้", "่", "า", "ส", "ว", "ง"],
+    ["ผ", "ป", "แ", "อ", "ิ", "ื", "ท", "ม", "ใ", "ฝ"],
+  ],
 };
 
 export class Layout {
-  public name: string;
+  public name: LayoutOptions["name"];
 
   constructor(options: LayoutOptions = { name: "pattachote" }) {
     this.name = options.name;
   }
 
   public get matrix() {
-    if (this.name === "kedmanee") {
-      return LAYOUTS.kedmanee;
-    }
-    return LAYOUTS.pattachote;
+    return LAYOUTS[this.name];
   }
 }
