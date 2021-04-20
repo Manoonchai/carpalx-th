1950e6b0ac947693d9a44546e26321f7305fad6d
Manassarn Manoonchai <manassarn@gmail.com>
2021-04-21 00:47:14 +0700

    :wrench: Reduce base "DH" effort

--- src/carpalx.ts
+++ src/carpalx.ts
@@ -25,7 +25,7 @@ const baseEffortMatrix = [
   [6, 6, 6, 6, 8, 10, 6, 6, 6, 6, 8, 10], // number row (larger)
   [2, 2, 2, 2, 2.5, 3, 2, 2, 2, 2, 2.5, 4, 6], //row 1
   [0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 2], //row 2 base
-  [2, 2, 2, 2, 3.5, 2, 2, 2, 2, 2], //down row
+  [2, 2, 2, 1, 3.5, 2, 1, 2, 2, 2], //down row
 ];
 
 export interface Triads {
