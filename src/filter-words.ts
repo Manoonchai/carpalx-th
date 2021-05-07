fe776b1b8070f5c7c95da268234d1cff2336d29b
Manassarn Manoonchai <manassarn@gmail.com>
2021-05-07 10:33:35 +0700

    :sparkles: Customize layout name for filter-words command

--- src/filter-words.ts
+++ src/filter-words.ts
@@ -2,7 +2,7 @@ import csv from "csv-parser"
 import fs from "fs"
 import { LayoutOptions, LAYOUTS } from "./layout"
 
-const layout: LayoutOptions["name"] = "manoonchai_v02"
+const layout: LayoutOptions["name"] = process.argv[2] as LayoutOptions["name"] || "manoonchai_v03"
 
 const allowedChars: string = LAYOUTS[layout][2].join("")
 const allowedCharsRegex = new RegExp(`^[${allowedChars}]+$`)
